e6973eeae4a465dfb4a7a29c7abcce9cd3cfd222
maluga-net <work@maluga.net>
2013-11-22 21:43:50 +0100

    + added initial test

---
+++ test/BasicsTest.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * test case.
+ */
+class BasicsTest extends PHPUnit_Framework_TestCase {
+	
+	public function testTrue() {
+		$this->assertTrue(true);
+	}
+
+}
